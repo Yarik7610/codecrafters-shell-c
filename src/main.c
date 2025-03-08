c1fc6c02d1420c7be863d62e85d76fd41dcaece0
Yaroslav Yanovich <arikarik782@gmail.com>
2025-03-08 15:40:41 +0300

    refactor: update your_program.sh to run shell locally

--- src/main.c
+++ src/main.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
   printf("$ ");
 
-  // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+
   return 0;
 }

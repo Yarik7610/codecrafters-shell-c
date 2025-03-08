cc8c7158da4ca6bdf8ffe512d4009afcf570e86b
Yaroslav Yanovich <arikarik782@gmail.com>
2025-03-08 16:06:46 +0300

    feat: add loop for REPL flow

--- src/main.c
+++ src/main.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_INPUT_SIZE 100
+
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
+  
+  char input[MAX_INPUT_SIZE];
 
-  printf("$ ");
+  while (1) {
+    printf("$ ");
 
-  char input[100];
-  fgets(input, 100, stdin);
+    fgets(input, MAX_INPUT_SIZE, stdin);
 
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
-
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }

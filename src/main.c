bbc244de7826563437d476400abfae34054f1eb7
Yaroslav Yanovich <arikarik782@gmail.com>
2025-03-08 16:39:10 +0300

    feat: add exit command

--- src/main.c
+++ src/main.c
@@ -3,19 +3,26 @@
 #include <string.h>
 
 #define MAX_INPUT_SIZE 100
+#define EXIT_COMMAND "exit 0"
 
-int main(int argc, char *argv[]) {
+int main() {
   setbuf(stdout, NULL);
   
   char input[MAX_INPUT_SIZE];
+  int input_len = 1;
 
   while (1) {
     printf("$ ");
 
     fgets(input, MAX_INPUT_SIZE, stdin);
 
-    input[strlen(input) - 1] = '\0';
+    input_len = strlen(input);
+    input[input_len - 1] = '\0';
+
+    if (strcmp(input, EXIT_COMMAND) == 0) exit(0);
+
     printf("%s: command not found\n", input);
   }
+
   return 0;
 }

e13183ab52f900d220ebc511c63d01f132bcdeb1
Yaroslav Yanovich <arikarik782@gmail.com>
2025-03-10 19:12:03 +0300

    fix: move finding command in env path to linux version

--- src/executables.c
+++ src/executables.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <io.h> 
+#include <unistd.h> 
 #include "executables.h"
 
 #define MAX_FULL_PATH_LENGTH 1024
-#define FILE_EXISTS 0
 
 char* get_command_from_env_path(char *command) {
   char *path = getenv("PATH");
@@ -18,14 +17,14 @@ char* get_command_from_env_path(char *command) {
     exit(1);
   }
  
-  char *splitted_path = strtok(path_copy, ";");
+  char *splitted_path = strtok(path_copy, ":");
 
   while (splitted_path != NULL) {
     char full_path[MAX_FULL_PATH_LENGTH]; 
 
-    snprintf(full_path, MAX_FULL_PATH_LENGTH, "%s\\%s", splitted_path, command);
+    snprintf(full_path, MAX_FULL_PATH_LENGTH, "%s/%s", splitted_path, command);
 
-    if (_access(full_path, FILE_EXISTS) != -1) {
+    if (access(full_path, X_OK) != -1) {
       free(path_copy);
 
       char *command_path = strdup(full_path);
@@ -37,7 +36,7 @@ char* get_command_from_env_path(char *command) {
       return command_path;
     }
 
-    splitted_path = strtok(NULL, ";");
+    splitted_path = strtok(NULL, ":");
   }
 
   free(path_copy);
